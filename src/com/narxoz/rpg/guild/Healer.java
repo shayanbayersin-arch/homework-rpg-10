db87b296d94fe91fef3534e70122d8d0c9145b76
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-05-06 16:51:42 +0500

    feat: implement Healer receive logic

--- src/com/narxoz/rpg/guild/Healer.java
+++ src/com/narxoz/rpg/guild/Healer.java
@@ -16,6 +16,6 @@ public class Healer extends GuildMember {
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.println("[Healer " + getName() + "] Readying kits for: " + payload);
     }
 }

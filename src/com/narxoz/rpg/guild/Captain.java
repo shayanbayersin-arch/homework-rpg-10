94043c2257c04d4f536f33f5e7ae9482ad14350e
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-05-06 16:37:53 +0500

    feat: implement Captain receive logic

--- src/com/narxoz/rpg/guild/Captain.java
+++ src/com/narxoz/rpg/guild/Captain.java
@@ -16,6 +16,6 @@ public class Captain extends GuildMember {
 
     @Override
     public void receive(String topic, GuildMember from, String payload) {
-        // TODO: react to a guild-hall message without calling another colleague directly.
+        System.out.println("[Captain " + getName() + "] Strategy update on " + topic + ": " + payload);
     }
 }
